72f28cee871b7f8f1be5732d78fbb4daaa8bbc43
Andres <andres.gar.gav@gmail.com>
2025-10-25 22:31:38 -0500

    [FIX] Fix leaderboard 2

--- src/features/portal/halloween/components/panels/HalloweenLeaderboard.tsx
+++ src/features/portal/halloween/components/panels/HalloweenLeaderboard.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { PortalLeaderboard } from "features/world/ui/portals/PortalLeaderboard";
-import * as AuthProvider from "features/auth/lib/Provider";
+// import * as AuthProvider from "features/auth/lib/Provider";
 // import { Context } from "features/game/GameProvider";
 import { PortalMachineState } from "../../lib/halloweenMachine";
 import { PortalContext } from "../../lib/PortalProvider";
@@ -14,7 +14,7 @@ const _jwt = (state: PortalMachineState) => state.context.jwt;
 export const HalloweenLeaderboard: React.FC = () => {
   const { portalService } = useContext(PortalContext);
   //   const { gameService } = useContext(Context);
-  const { authService } = useContext(AuthProvider.Context);
+  //   const { authService } = useContext(AuthProvider.Context);
 
   const jwt = useSelector(portalService, _jwt);
 
@@ -26,7 +26,7 @@ export const HalloweenLeaderboard: React.FC = () => {
       startDate={new Date(Date.UTC(2025, 5, 30))}
       endDate={new Date(Date.UTC(2025, 6, 6))}
       farmId={Number(farmId)}
-      jwt={authService.getSnapshot().context.user.rawToken as string}
+      jwt={jwt as string}
     />
   );
 };
