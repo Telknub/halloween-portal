62421576a3fe2ff3fdcfc5b980ca13132cf94252
Andres <andres.gar.gav@gmail.com>
2025-10-26 23:21:18 -0500

    [FIX] Update leaderboard

--- src/features/portal/halloween/components/panels/HalloweenLeaderboard.tsx
+++ src/features/portal/halloween/components/panels/HalloweenLeaderboard.tsx
@@ -23,7 +23,7 @@ export const HalloweenLeaderboard: React.FC = () => {
   return (
     <PortalLeaderboard
       name={PORTAL_NAME}
-      startDate={new Date(Date.UTC(2025, 9, 28))}
+      startDate={new Date(Date.UTC(2025, 9, 25))}
       endDate={new Date(Date.UTC(2025, 10, 3))}
       farmId={Number(farmId)}
       jwt={jwt as string}
